9265119f5f75a195e254927a63dac1dc3908a01f
Adit <adit.shukla@gmail.com>
2024-09-12 12:53:47 -0400

    Trying again

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/staging/' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/staging' : '',
   images: {
     unoptimized: true,
   },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/staging/' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/staging' : '',
 }
 
 module.exports = nextConfig
