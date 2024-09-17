e52c445f0c7aadf92328bd7bb62ff92b47d8ac93
Adit <adit.shukla@gmail.com>
2024-09-17 14:15:44 -0400

    Attempting tailwind

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'https://rubygems.org'
+
+ruby '2.6.3'
+
+gem 'jekyll'
+gem 'webrick'
+gem 'jekyll-postcss'
+
+# Additional gem declarations
+
+gem "minima", "~> 2.5"
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./_includes/**/*.html",
+    "./_layouts/**/*.html",
+    "./index.md",
+    "./_sub-services/**/*.md",
+    "./assets/**/*.{js,scss}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
